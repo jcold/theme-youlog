37a680e72c807374142d58632f0e360126698aef
dayu <dayu@everkm.cn>
2025-06-24 18:02:11 +0800

    显示版本列表

--- __everkm/theme/youlog/src/VersionList.tsx
+++ __everkm/theme/youlog/src/VersionList.tsx
@@ -1,20 +1,43 @@
-import { Component, createSignal, JSX, onMount } from "solid-js";
+import {
+  Component,
+  createSignal,
+  JSX,
+  onMount,
+  onCleanup,
+  createEffect,
+  Setter,
+  Accessor,
+} from "solid-js";
 import { customElement, noShadowDOM } from "solid-element";
+import { Popover } from "@kobalte/core/popover";
+import "./popover.css";
 
 const VersionList = (props: VersionListProps) => {
   const [versionList, setVersionList] = createSignal<any[]>([]);
 
-//   onMount(() => {
-//     fetch(props.versionListUrl || "")
-//       .then((res) => res.json())
-//       .then((data) => setVersionList(data));
-//   });
-
-  return <div>{props.children}</div>;
+  return (
+    <>
+      <Popover>
+        <Popover.Trigger class="popover__trigger">
+          v{props.version}
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content class="popover__content">
+            <Popover.Arrow />
+            <Popover.Description class="popover__description">
+              A UI toolkit for building accessible web apps and design systems
+              with SolidJS.
+            </Popover.Description>
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover>
+    </>
+  );
 };
 
 interface VersionListProps extends JSX.DOMAttributes<HTMLElement> {
   versionListUrl?: string;
+  version: string;
 }
 
 // 注册自定义元素
@@ -22,6 +45,7 @@ customElement(
   "youlog-version",
   {
     versionListUrl: "",
+    version: "",
   },
   (props: VersionListProps) => {
     noShadowDOM();
